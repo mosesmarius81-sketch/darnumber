b58833612a68e1e4781aa40262de6203f8f6a960
Mosesedem <mosesedem81@gmail.com>
2026-05-19 14:06:34 +0100

    refactor: remove unused OpenNext.js Cloudflare configurations and headers

--- next.config.ts
+++ next.config.ts
@@ -18,4 +18,4 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+// import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
