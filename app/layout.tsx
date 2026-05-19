60e7422ba6868961e68787e84068612c0ffbc8cc
Mosesedem <mosesedem81@gmail.com>
2026-05-19 18:57:38 +0100

    fix: restore children rendering in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {/* {children} */}
-          <DeploymentPaused />
+          {children}
+          {/* <DeploymentPaused /> */}
           <Toaster />
         </AuthProvider>
       </body>
